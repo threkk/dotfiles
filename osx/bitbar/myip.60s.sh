0e3c990ec968ddb81e35739cea25343eccff9e1e
Alberto Martinez de Murga Ramirez <alberto@threkk.com>
2022-11-24 00:42:44 +0100

    chore: dotfiles cleanup

--- osx/bitbar/myip.60s.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# <bitbar.title>MyIP</bitbar.title>
-# <bitbar.version>dev</bitbar.version>
-# <bitbar.author>Alberto Martinez de Murga Ramirez</bitbar.author>
-# <bitbar.author.github>threkk</bitbar.author.github>
-# <bitbar.desc>Returns the IP addresses of the system.</bitbar.desc>
-echo "MyIP"
-echo "---"
-/Users/threkk/Documents/projects/bin/myip -public -local
